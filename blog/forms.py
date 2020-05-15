6d667a1646a951895323f34fe927f7d614545afe
saper663_home <saper663ru@gmail.com>
2020-05-15 20:33:38 +0500

    made a display of the comment form and a link with the anchor to the comments from the page base.html

--- blog/forms.py
+++ blog/forms.py
@@ -1,4 +1,5 @@
 from django import forms
+from django.forms import TextInput, Textarea
 from .models import Comment
 
 
@@ -13,4 +14,25 @@ class CommentForm(forms.ModelForm):
     class Meta:
         model = Comment
         fields = ('name', 'email', 'body')
+        widgets = {
+            'name' : TextInput(attrs={'type' : 'text', 
+                                     'class' : 'form-control',
+                                     'id' : 'name',
+                                     'placeholder' : 'Enter Name',
+                                     'onfocus' : "this.placeholder = ''",
+                                     'onblur' : "this.placeholder = 'Enter Name'"}),
+            'email' : TextInput(attrs={'type' : 'email',
+                                       'class' : 'form-control',
+                                       'id' : 'email',
+                                       'placeholder' : 'Enter email address',
+                                       'onfocus' : 'this.placeholder = ""',
+                                       'onblur' : 'this.placeholder = "Enter email address"'}),
+            'body' : Textarea(attrs={'class' : 'form-control mb-10',
+                                     'rows' : '5',
+                                     'name' : 'message',
+                                     'placeholder' : 'Messege',
+                                     'onfocus' : 'this.placeholder = ""',
+                                     'onblur' : 'this.placeholder = "Messege"',
+                                     'required' : ""})                                  
+        }
         
